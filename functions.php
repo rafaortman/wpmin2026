0a2074989d843c19aa328607036b9b48be7f3eb3
Rafa Ortman <rafaortman@gmail.com>
2026-02-18 17:34:58 -0300

    Refactor theme_cleanup to improve style removal

--- functions.php
+++ functions.php
@@ -23,44 +23,42 @@
     add_action('wp_enqueue_scripts','theme_enqueue_assets');
 
 	function theme_cleanup() {
-		// ========================
-		// REMOVER ESTILOS INLINE DO WP CORE
-		// ========================
-		remove_action('wp_enqueue_scripts', 'wp_enqueue_global_styles'); // global-styles-inline-css
-		wp_dequeue_style('classic-theme-styles'); // classic-theme-styles-inline-css
-		wp_dequeue_style('wp-block-library'); // block library
-		wp_dequeue_style('wp-block-library-theme');
-		wp_dequeue_style('wp-block-library-rtl');
-		wp_dequeue_style('wp-components'); // Gutenberg components
-
-		// Remove Dashicons no front-end para usuários não logados
-		if(!is_admin() && !is_user_logged_in()) {
+		if (!is_admin() && !is_user_logged_in() && $GLOBALS['pagenow'] !== 'wp-login.php') {
+			// ========================
+			// REMOVER ESTILOS INLINE DO WP CORE
+			// ========================
+			remove_action('wp_enqueue_scripts', 'wp_enqueue_global_styles'); // global-styles-inline-css
+			wp_dequeue_style('classic-theme-styles'); // classic-theme-styles-inline-css
+			wp_dequeue_style('wp-block-library'); // block library
+			wp_dequeue_style('wp-block-library-theme');
+			wp_dequeue_style('wp-block-library-rtl');
+			wp_dequeue_style('wp-components'); // Gutenberg components
 			wp_deregister_style('dashicons');
+
+			// Evita que WP injete auto-size inline nas imagens
+			add_filter('wp_img_tag_add_sizes_attr', '__return_false');
+
+
+			// ========================
+			// REMOVE EMOJI (HEAD E FOOTER)
+			// ========================
+			remove_action('wp_head', 'print_emoji_detection_script', 7);
+			remove_action('wp_print_scripts', 'print_emoji_detection_script', 7);
+			remove_action('admin_print_scripts', 'print_emoji_detection_script');
+			remove_action('wp_print_styles', 'print_emoji_styles');
+			remove_action('admin_print_styles', 'print_emoji_styles');
+			add_filter('emoji_svg_url', '__return_false'); // remove JSON e loader module
+
+			// ========================
+			// REMOVE LINKS E META DESNECESSÁRIOS DO HEAD
+			// ========================
+			remove_action('wp_head', 'wp_generator'); // meta generator
+			remove_action('wp_head', 'rsd_link'); // RSD
+			remove_action('wp_head', 'wlwmanifest_link'); // Windows Live Writer
+			remove_action('wp_head', 'wp_shortlink_wp_head'); // shortlink
+			remove_action('wp_head','rest_output_link_wp_head'); // REST API link
+			remove_action('wp_head','wp_oembed_add_discovery_links'); // oEmbed links
 		}
-
-		// Evita que WP injete auto-size inline nas imagens
-		add_filter('wp_img_tag_add_sizes_attr', '__return_false');
-
-
-		// ========================
-		// REMOVE EMOJI (HEAD E FOOTER)
-		// ========================
-		remove_action('wp_head', 'print_emoji_detection_script', 7);
-		remove_action('wp_print_scripts', 'print_emoji_detection_script', 7);
-		remove_action('admin_print_scripts', 'print_emoji_detection_script');
-		remove_action('wp_print_styles', 'print_emoji_styles');
-		remove_action('admin_print_styles', 'print_emoji_styles');
-		add_filter('emoji_svg_url', '__return_false'); // remove JSON e loader module
-
-		// ========================
-		// REMOVE LINKS E META DESNECESSÁRIOS DO HEAD
-		// ========================
-		remove_action('wp_head', 'wp_generator'); // meta generator
-		remove_action('wp_head', 'rsd_link'); // RSD
-		remove_action('wp_head', 'wlwmanifest_link'); // Windows Live Writer
-		remove_action('wp_head', 'wp_shortlink_wp_head'); // shortlink
-		remove_action('wp_head','rest_output_link_wp_head'); // REST API link
-		remove_action('wp_head','wp_oembed_add_discovery_links'); // oEmbed links
 	}
 	add_action('wp_enqueue_scripts', 'theme_cleanup', 100);
 	add_action('init', 'theme_cleanup');
