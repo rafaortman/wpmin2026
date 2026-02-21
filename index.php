8871535944c506ffb73f3666ee16336f0b82d5f4
Rafa Ortman <rafaortman@gmail.com>
2026-02-21 14:10:48 -0300

    Improve post handling and error display in index.php
    Added error handling for no posts and updated title display.

--- index.php
+++ index.php
@@ -3,20 +3,23 @@
 
 <main class="index">
     <div class="max680">
-        <h2 class="the_title">
-            <?= get_the_title( get_option('page_for_posts') ); ?>
-        </h2>
         <?php 
 			if ( have_posts() ) {
+				echo '<h2 class="the_title">' . get_the_title( get_option('page_for_posts') ) . '</h2>'; 
 				echo '<div class="the_posts">';
 					while( have_posts()) : the_post(); 
 						get_template_part( 'includes/post-inside-loop' ); 
 					endwhile; 
 				echo '</div>';
-			} 
+			} else {
+				echo '<h2 class="the_title">' . carbon_get_theme_option('erro404_titulo') . '</h2>
+				<article>' . carbon_get_theme_option('erro404_conteudo') . '
+					<br><br>
+					<a href="' . home_url() . '" class="btn">Página Inicial</a>
+				</article>';
+			}
 		?>
     </div>
 </main>
 
 <?php get_footer(); ?>
-
